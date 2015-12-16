a56428b88a4cbb04507f96cc2e43396b5a7fad73
Alex Ballera <alexballera@gmail.com>
2015-12-16 16:24:32 -0430

    Agregado video responsive

--- app/scripts/js/main.js
+++ app/scripts/js/main.js
@@ -1,5 +1,6 @@
 (function (){
   'use strict';
+  //Menú
   var btnMenu = document.getElementById('btnMenu');
   var navbarMenu = document.getElementById('navbarMenu');
 
@@ -8,5 +9,14 @@
   function onClickMenu(){
     navbarMenu.classList.toggle('header__menu--list--show');
     navbarMenu.classList.toggle('header__menu--list');
+  }
+  // Video
+  var v = document.getElementsByTagName("video")[0];
+  var video = document.getElementById('video');
+  
+  video.addEventListener('click', initVideo);
+  
+  function initVideo(){
+    v.play()
   }
 }());
