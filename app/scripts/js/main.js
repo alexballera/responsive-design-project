091ec6ba77835957a63253c7cabd1c78a7f5e034
Alex Ballera <alexballera@gmail.com>
2015-12-17 00:43:01 -0430

    Actualizando

--- app/scripts/js/main.js
+++ app/scripts/js/main.js
@@ -1,23 +1,40 @@
 (function (){
   'use strict';
-  //Menú
-  var btnMenu = document.getElementById('btnMenu');
-  var navbarMenu = document.getElementById('navbarMenu');
+  document.addEventListener('DOMContentLoaded', onDOMLoad);
 
-  btnMenu.addEventListener('click', onClickMenu);
-  
-  function onClickMenu(){
-    navbarMenu.classList.toggle('header__menu--list--show');
-    navbarMenu.classList.toggle('header__menu--list');
-  }
-  // Video
-  var v = document.getElementsByTagName('video')[0];
-  var video = document.getElementById('video');
-  
-  video.addEventListener('click', onClickVideo);
-  
-  function onClickVideo(){
-    video.setAttribute('controls', 'controls');
-    v.play();
+  function onDOMLoad(){
+    //Variables Globales
+    var btnMenu = document.getElementById('btnMenu');
+    var navbarMenu = document.getElementById('navbarMenu');
+    var video = document.getElementsByTagName('video')[0];
+    var videoVideo = document.getElementById('video');
+    var headerElem = document.querySelector('header');
+
+    //Menú
+    btnMenu.addEventListener('click', onClickMenu);
+    
+    function onClickMenu(){
+      navbarMenu.classList.toggle('header__menu--list--show');
+      navbarMenu.classList.toggle('header__menu--list');
+    }
+    // Video
+    
+    videoVideo.addEventListener('click', onClickVideo);
+    
+    function onClickVideo(){
+      videoVideo.setAttribute('controls', 'controls');
+      video.play();
+    }
+
+    //Scroll
+    headerElem.addEventListener('scroll', onScroll);
+
+    function onScroll(){
+      if(window.scrollY >= 200) {
+        headerElem.classList.toggle('.header__light');
+      }else{
+        headerElem.classList.toggle('.header__light');
+      }
+    }
   }
 }());
